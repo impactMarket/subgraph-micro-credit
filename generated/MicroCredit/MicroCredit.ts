cedbc5addd5555d12c983371ab015d5499b0e7f2
Bernardo Vieira <bernardo@impactmarket.com>
2023-03-23 11:19:24 -0300

    draft initial global data indexing

--- generated/MicroCredit/MicroCredit.ts
+++ generated/MicroCredit/MicroCredit.ts
@@ -10,25 +10,103 @@ import {
   BigInt
 } from "@graphprotocol/graph-ts";
 
-export class BorrowerRepay extends ethereum.Event {
-  get params(): BorrowerRepay__Params {
-    return new BorrowerRepay__Params(this);
+export class LoanAdded extends ethereum.Event {
+  get params(): LoanAdded__Params {
+    return new LoanAdded__Params(this);
   }
 }
 
-export class BorrowerRepay__Params {
-  _event: BorrowerRepay;
+export class LoanAdded__Params {
+  _event: LoanAdded;
 
-  constructor(event: BorrowerRepay) {
+  constructor(event: LoanAdded) {
     this._event = event;
   }
 
-  get borrower(): Address {
+  get userAddress(): Address {
     return this._event.parameters[0].value.toAddress();
   }
 
   get amount(): BigInt {
     return this._event.parameters[1].value.toBigInt();
+  }
+
+  get period(): BigInt {
+    return this._event.parameters[2].value.toBigInt();
+  }
+
+  get dailyInterest(): BigInt {
+    return this._event.parameters[3].value.toBigInt();
+  }
+}
+
+export class LoanClaimed extends ethereum.Event {
+  get params(): LoanClaimed__Params {
+    return new LoanClaimed__Params(this);
+  }
+}
+
+export class LoanClaimed__Params {
+  _event: LoanClaimed;
+
+  constructor(event: LoanClaimed) {
+    this._event = event;
+  }
+
+  get userAddress(): Address {
+    return this._event.parameters[0].value.toAddress();
+  }
+
+  get loanId(): BigInt {
+    return this._event.parameters[1].value.toBigInt();
+  }
+}
+
+export class RepaymentAdded extends ethereum.Event {
+  get params(): RepaymentAdded__Params {
+    return new RepaymentAdded__Params(this);
+  }
+}
+
+export class RepaymentAdded__Params {
+  _event: RepaymentAdded;
+
+  constructor(event: RepaymentAdded) {
+    this._event = event;
+  }
+
+  get userAddress(): Address {
+    return this._event.parameters[0].value.toAddress();
+  }
+
+  get loanId(): BigInt {
+    return this._event.parameters[1].value.toBigInt();
+  }
+
+  get currentDebt(): BigInt {
+    return this._event.parameters[2].value.toBigInt();
+  }
+}
+
+export class UserAddressChanged extends ethereum.Event {
+  get params(): UserAddressChanged__Params {
+    return new UserAddressChanged__Params(this);
+  }
+}
+
+export class UserAddressChanged__Params {
+  _event: UserAddressChanged;
+
+  constructor(event: UserAddressChanged) {
+    this._event = event;
+  }
+
+  get oldWalletAddress(): Address {
+    return this._event.parameters[0].value.toAddress();
+  }
+
+  get newWalletAddress(): Address {
+    return this._event.parameters[1].value.toAddress();
   }
 }
 
--- src/mappings/micro-credit.ts
+++ src/mappings/micro-credit.ts
@@ -1,8 +1,76 @@
-// import { Address, Bytes, store } from '@graphprotocol/graph-ts';
+import { Address, BigDecimal, BigInt } from '@graphprotocol/graph-ts';
+import { Asset, MicroCredit } from '../../generated/schema';
 import {
-    BorrowerRepay
+    LoanAdded
 } from '../../generated/MicroCredit/MicroCredit';
 
-export function handleBorrowerRepay(event: BorrowerRepay): void {
-    //
+const cUSD = '0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1';
+
+export const normalize = (amount: string): BigDecimal =>
+    BigDecimal.fromString(amount).div(BigDecimal.fromString('1000000000000000000'));
+
+/**
+ * Generic asset entity update. It updates de array linked to the main entity and saves it.
+ * @param {string} assetId Asset id
+ * @param {string} asset Asset address
+ * @param {BigInt} amount Amount to be added or subtracted
+ * @param {Array<string> | null} array Array to be updated
+ * @param {boolean} isMinus Boolean to indicate if the amount should be subtracted or added
+ * @returns {Array<string>} Returns updated array
+ */
+function updateAsset(assetId: string, asset: string, amount: BigInt, array: Array<string> | null, isMinus: boolean): Array<string> {
+    let assetUpdated = Asset.load(assetId);
+
+    // create asset entity if it doesn't exist
+    if (!assetUpdated) {
+        assetUpdated = new Asset(assetId);
+        assetUpdated.amount = BigDecimal.zero();
+    }
+
+    // update new asset entity data
+    assetUpdated.asset = Address.fromString(asset);
+    if (isMinus) {
+        assetUpdated.amount = assetUpdated.amount.minus(normalize(amount.toString()));
+    } else {
+        assetUpdated.amount = assetUpdated.amount.plus(normalize(amount.toString()));
+    }
+
+    let newArray = array;
+
+    // create array if it doesn't exist
+    if (!newArray) {
+        newArray = new Array<string>();
+    }
+
+    newArray.push(assetId);
+
+    // save newly created asset entity
+    assetUpdated.save();
+
+    return newArray;
+}
+
+export function handleLoanAdded(event: LoanAdded): void {
+    // load or create new global micro credit entity
+    let microCredit = MicroCredit.load('0');
+
+    if (!microCredit) {
+        microCredit = new MicroCredit('0');
+        microCredit.borrowers = 0;
+        microCredit.repayments = 0;
+    }
+
+    // update global micro credit entity data
+    const assetBorrowedId = `borrowed-${cUSD}-0`;
+    const assetDebtId = `debt-${cUSD}-0`;
+    const assetLiquidityId = `liquidity-${cUSD}-0`;
+
+    microCredit.borrowed = updateAsset(assetBorrowedId, cUSD, event.params.amount, microCredit.borrowed, false);
+    microCredit.debt = updateAsset(assetDebtId, cUSD, event.params.amount, microCredit.debt, false);
+    microCredit.liquidity = updateAsset(assetLiquidityId, cUSD, event.params.amount, microCredit.liquidity, true);
+    microCredit.borrowers += 1;
+
+    microCredit.save();
+
+    // TBA: update borrower entity
 }
